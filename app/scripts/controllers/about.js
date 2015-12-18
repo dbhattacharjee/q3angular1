56a1b6ad5304ea875484a3896b9db60deffb3b5a
Dipanjan Bhattacharjee <dipanjan.bhattacharjee@myglobalit.com>
2015-12-18 18:35:18 +0530

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name heroku1App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the heroku1App
+ */
+angular.module('heroku1App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
