05f68a01d094341d478565a33f1fd2a29446411d
Dipanjan Bhattacharjee <dipanjan.bhattacharjee@myglobalit.com>
2015-12-21 17:41:02 +0530

    Added login html

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name heroku1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the heroku1App
+ */
+angular.module('heroku1App')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
