2d1f1c62ae7994cbb9c4f4066817c8e5911821fc
Dipanjan Bhattacharjee <dipanjan.bhattacharjee@myglobalit.com>
2015-12-30 19:24:09 +0530

    Working on read mail feature

--- app/scripts/controllers/mail.js
+++ app/scripts/controllers/mail.js
@@ -22,6 +22,21 @@
                     }
                 });
         };
+        
+        $rootScope.readMail = function(mailUID) {
+            vm.dataLoading = true;
+            UserService.GetSingleMail(mailUID)
+                .then(function (response) {
+                    if (response.data.success) {
+                        FlashService.Success('Emails Fetched !', true);
+                        $rootScope.mails = response.data.mails;
+                        vm.dataLoading = false;
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        };
         register();
     }
 
